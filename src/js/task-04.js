b14f3ec6d66a25753c1a1f801ce69a7ac0ee69e5
Freebo12 <Tumechko12@gmail.com>
2022-12-10 16:20:46 +0100

    done 4 task

---
+++ src/js/task-04.js
@@ -0,0 +1,22 @@
+const buttonjs = document.querySelectorAll("button");
+
+buttonjs.forEach(function (btn) {
+  btn.addEventListener("click", onButtonClick);
+
+  function onButtonClick() {
+    const action = this.dataset.action;
+    const spanJs = document.querySelector("#value");
+
+    const currentValue = spanJs.textContent;
+    let counterValue = 0;
+
+    if (action === "increment") {
+      for (let i = 0; i < 2; i++) {
+        counterValue = +currentValue + +i;
+      }
+    } else {
+      counterValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+    }
+    return (spanJs.textContent = counterValue);
+  }
+});
