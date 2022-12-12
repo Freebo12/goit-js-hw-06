00b0172e3cd8ed117fd647bebb0546a26d82d426
Freebo12 <Tumechko12@gmail.com>
2022-12-12 19:00:31 +0100

    8 task done !!!!!!!!!!!!!!

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formJs = document.querySelector(".login-form");
 
+formJs.addEventListener("submit", onManageSubmit);
+
+function onManageSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  } else {
+    const userData = {
+      email: `${email.value}`,
+      password: `${password.value}`,
+    };
+    event.currentTarget.reset();
+    console.log(userData);
+  }
+}
