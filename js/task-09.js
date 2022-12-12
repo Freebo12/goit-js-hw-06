8afa6bc3552c5184e13c4b331257723b89d6e3d0
Freebo12 <Tumechko12@gmail.com>
2022-12-12 20:38:46 +0100

    9 task done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyJs = document.querySelector("body");
+const btnJs = document.querySelector(".change-color");
+const spanJs = bodyJs.querySelector(".color");
+
+btnJs.addEventListener("click", changeBackground);
+
+function changeBackground() {
+  spanJs.textContent = bodyJs.style.backgroundColor = getRandomHexColor();
+}
