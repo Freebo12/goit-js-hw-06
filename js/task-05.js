a8dd1a610dedfe3284fa3623fcad3c709b5d4a5a
Freebo12 <Tumechko12@gmail.com>
2022-12-12 20:55:20 +0100

    fixed mistakes

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,5 @@
 const inputJs = document.querySelector("#name-input");
-console.log(inputJs);
-
 const spanJs = document.querySelector("#name-output");
-console.log(spanJs);
 
 inputJs.addEventListener("input", (event) => {
   if (inputJs.value === "") {
